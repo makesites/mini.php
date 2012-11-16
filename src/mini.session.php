cfd23b63034d8ab2a8e74488c509a91a6829f124
Makis Tracend <makis.tracend@gmail.com>
2012-11-15 20:25:32 -0800

    New mini: session()

---
+++ src/mini.session.php
@@ -0,0 +1,60 @@
+<?
+/*
+ * mini.PHP
+ * - Session() class
+ *
+ * Created by Makis Tracend (@tracend) 
+ * Maintained by Make Sites (makesites.org)
+ */
+class miniSession extends miniBase { 
+	private $key;
+	
+	function __construct( $vars=false, $key=false ){ 
+		
+		// default variables
+		$this->vars = array();
+		
+		if( $vars ) $this->vars = array_merge( $this->vars, $vars);
+		
+		$this->start();
+		
+		// populate with a specific set (or get the whole session)
+		$this->key = ( $key ) ? $key : false; 
+		
+		$this->vars = array_merge( $this->vars, $this->session());
+		
+	}
+	
+	private function start() {
+		// Check if there's a valid session
+		if (strlen(session_id()) < 1) {
+			session_start();
+		}
+		
+	}
+	
+	public function destroy(){
+		 session_destroy();
+	}
+	
+	public function exists(){
+		return (strlen(session_id()) >= 1);
+	}
+	
+	public function set($key, $val) {
+		if( $this->key ) {
+			$_SESSION[$this->key][$key] = $val;
+		} else {
+			$_SESSION[$key] = $val;
+		}
+		return parent::set($key, $val);
+	}
+	
+	private function session(){
+		// return the whole session if no key specified
+		return ($this->key) ? $_SESSION[$this->key] : $_SESSION;
+	}
+	
+}
+
+?>

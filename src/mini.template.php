2a8219d4aaf2f19f5ebd0244825eb9e7a656bb20
Makis Tracend <makis.tracend@gmail.com>
2013-11-04 21:50:07 -0800

    adding views & data containers in template class

--- src/mini.template.php
+++ src/mini.template.php
@@ -3,53 +3,55 @@
  * mini.PHP
  * - Template() class
  *
- * Created by Makis Tracend (@tracend) 
+ * Created by Makis Tracend (@tracend)
  * Published by Make Sites (makesites.org)
  */
 class miniTemplate extends miniBase {
-	
+
 	function __construct( $vars=false ){
-		
+
 		// default variables
 		$this->vars = array(
-			"path" => $_SERVER['DOCUMENT_ROOT'] . '/templates/', 
-			"layout" => "layout", 
+			"path" => $_SERVER['DOCUMENT_ROOT'] . '/templates/',
+			"layout" => "layout",
 			"ext" => "php"
 		);
-		
+		// containers
+		$this->views = array();
+		$this->data = array();
+
 		if( $vars ) $this->vars = array_merge( $this->vars, $vars);
-		
+
 	}
-	
+
 	public function render(){
-		
+
 		// get the view first
-		$this->vars["body"] = $this->fetch( $this->vars['view'] );
-		
+		$this->views["body"] = $this->fetch( $this->vars['view'] );
+
 		// load the layout
 		$template = $this->fetch( $this->vars['layout'] );
-		
+
 		//var_dump($template);
-		
+
 		return $template;
-	} 
-	
+	}
+
 	// Helpers
-	
-	private function fetch( $name ){
+
+	private function fetch( $name, $data=array() ){
 		$file = $this->vars['path'] . $name .'.'. $this->vars['ext'];
 		// exit now if there is no file
 		if( !is_file($file) ) return;
 		// extract variables
-		if (is_array($this->vars))
-			extract($this->vars);
+		$this->data = array_merge( $this->data, $data);
+		if (is_array($this->data))
+			extract($this->data);
 		ob_start();
 		require($file);
 		return ob_get_clean();
-		//$content = file_get_contents( $file );
-		//return $content;
 	}
-	
+
 }
 
 ?>
